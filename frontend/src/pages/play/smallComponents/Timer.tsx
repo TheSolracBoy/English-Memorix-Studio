e45b01f17a64936f85f576b67d98a8eff64126d3
charlieman2700 <carlos.ace27@gmail.com>
2024-02-09 12:12:32 -0600

    feat: :sparkles: Add music to the game

--- frontend/src/pages/play/smallComponents/Timer.tsx
+++ frontend/src/pages/play/smallComponents/Timer.tsx
@@ -1,23 +1,23 @@
 
 interface TimerProps {
-	seconds: number
+  seconds: number
 }
 export default function Timer({ seconds }: TimerProps) {
 
-	const formatTime = () => {
-		const minutes = Math.floor(seconds / 60);
-		const remainingSeconds = seconds % 60;
+  const formatTime = () => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
 
-		// Use String.prototype.padStart to ensure two-digit format
-		const formattedMinutes = String(minutes).padStart(2, '0');
-		const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+    // Use String.prototype.padStart to ensure two-digit format
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
 
-		return `${formattedMinutes}:${formattedSeconds}`;
-	};
+    return `${formattedMinutes}:${formattedSeconds}`;
+  };
 
-	return (
-		<div className="border-2 rounded-md px-2 py-1 w-24 ">
-			<p className="text-2xl text-center">{formatTime()}</p>
-		</div>
-	);
+  return (
+    <div className="border-2 border-cyan-100 rounded-md px-2 py-1 w-24 ">
+      <p className="text-2xl text-center ">{formatTime()}</p>
+    </div>
+  );
 }
